9e3f31654bb3d5485a6f2b99edb514a92cfb5e91
Richard Wild <richardwild76@gmail.com>
2017-11-15 15:00:14 +0000

    Now make the walking skeleton go green

--- src/main/java/com/github/richardjwild/blather/Blather.java
+++ src/main/java/com/github/richardjwild/blather/Blather.java
@@ -3,6 +3,6 @@ package com.github.richardjwild.blather;
 public class Blather {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Blither!");
+        System.out.println("Welcome to Blather!");
     }
 }
