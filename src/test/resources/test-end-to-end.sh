3cf1d7d886da103a457377dda6e99bfe2d7e10ff
Richard Wild <richardwild76@gmail.com>
2017-11-20 09:30:56 +0000

    Corrected working directory

--- src/test/resources/test-end-to-end.sh
+++ src/test/resources/test-end-to-end.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [ ! -f build/libs/blather.jar ]; then
+WORKING_DIRECTORY=$1
+BLATHER_EXECUTABLE=${WORKING_DIRECTORY}/build/libs/blather.jar
+
+echo "Checking for Blather executable at: ${BLATHER_EXECUTABLE}"
+if [ ! -f ${WORKING_DIRECTORY}/build/libs/blather.jar ]; then
     echo "Test failed! Blather executable was not found"
     exit 1
 fi
@@ -8,7 +12,7 @@ fi
 EXPECTED="Welcome to Blather
 Bye!"
 
-ACTUAL=`echo quit | java -cp build/libs/blather.jar com.github.richardjwild.blather.Blather`
+ACTUAL=`echo quit | java -cp ${WORKING_DIRECTORY}/build/libs/blather.jar com.github.richardjwild.blather.Blather`
 
 if [ $? -ne 0 ]; then
     echo "Test failed! Error running Blather executable"
