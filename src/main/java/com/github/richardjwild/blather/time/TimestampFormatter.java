fbcceb27a3a50c7c37a0a572478f46a101d1d911
Richard Wild <richardwild76@gmail.com>
2017-11-20 10:22:13 +0000

    Timestamp formatter will format seconds

--- src/main/java/com/github/richardjwild/blather/time/TimestampFormatter.java
+++ src/main/java/com/github/richardjwild/blather/time/TimestampFormatter.java
@@ -2,13 +2,22 @@ package com.github.richardjwild.blather.time;
 
 import java.time.Instant;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+
 public class TimestampFormatter {
 
-    public TimestampFormatter(Clock clock) {
+    private final Clock clock;
 
+    public TimestampFormatter(Clock clock) {
+        this.clock = clock;
     }
 
     public String format(Instant timestamp) {
-        return "(0 seconds ago)";
+        long secondsDifference = SECONDS.between(timestamp, clock.now());
+        return String.format("(%d %s ago)", secondsDifference, pluralize("second", secondsDifference));
+    }
+
+    private String pluralize(String unit, long i) {
+        return i == 1 ? unit : unit + "s";
     }
 }
--- src/test/java/com/github/richardjwild/blather/time/TimestampFormatterShould.java
+++ src/test/java/com/github/richardjwild/blather/time/TimestampFormatterShould.java
@@ -1,5 +1,6 @@
 package com.github.richardjwild.blather.time;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -7,6 +8,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.time.Instant;
 
+import static java.time.Instant.now;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -16,15 +18,40 @@ public class TimestampFormatterShould {
     @Mock
     private Clock clock;
 
+    private TimestampFormatter timestampFormatter;
+
+    @Before
+    public void initialize() {
+        timestampFormatter = new TimestampFormatter(clock);
+    }
+
     @Test
     public void format_a_timestamp_from_just_now_as_0_seconds_ago() {
-        Instant timestamp = Instant.now();
+        Instant timestamp = now();
         when(clock.now()).thenReturn(timestamp);
-        TimestampFormatter timestampFormatter = new TimestampFormatter(clock);
 
         String formattedTimestamp = timestampFormatter.format(timestamp);
 
         assertThat(formattedTimestamp).isEqualTo("(0 seconds ago)");
     }
 
+    @Test
+    public void format_a_timestamp_from_1_second_ago_as_1_second_ago() {
+        Instant timestamp = now();
+        when(clock.now()).thenReturn(timestamp.plusSeconds(1));
+
+        String formattedTimestamp = timestampFormatter.format(timestamp);
+
+        assertThat(formattedTimestamp).isEqualTo("(1 second ago)");
+    }
+
+    @Test
+    public void format_a_timestamp_from_2_seconds_ago_as_2_second_ago() {
+        Instant timestamp = now();
+        when(clock.now()).thenReturn(timestamp.plusSeconds(2));
+
+        String formattedTimestamp = timestampFormatter.format(timestamp);
+
+        assertThat(formattedTimestamp).isEqualTo("(2 seconds ago)");
+    }
 }
