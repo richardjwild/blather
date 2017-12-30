335c5153bf902a74cf10039c91e32da3322de432
Richard Wild <richardwild76@gmail.com>
2017-12-30 08:26:09 +0000

    Refactored ReadCommandShould

--- src/test/java/com/github/richardjwild/blather/command/ReadCommandShould.java
+++ src/test/java/com/github/richardjwild/blather/command/ReadCommandShould.java
@@ -13,16 +13,17 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.stream.Stream;
 
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReadCommandShould {
 
-    private static final User USER = new User("user");
+    private static final String TEXT = "this test does not depend on this text";
+    private static final Instant AN_INSTANT_IN_TIME = Instant.now();
+    private static final Instant ANOTHER_INSTANT_IN_TIME = AN_INSTANT_IN_TIME.plusSeconds(1);
 
     @Mock
     private Output output;
@@ -38,30 +39,34 @@ public class ReadCommandShould {
 
     @Test
     public void print_all_messages_posted_to_a_specified_user_in_date_order() {
-        Instant timestamp = Instant.now();
-        Message message1 = new Message(USER, null, timestamp);
-        Message message2 = new Message(USER, null, timestamp.plusSeconds(1));
-        when(userRepository.find("user")).thenReturn(of(USER));
-        when(messageRepository.allMessagesPostedTo(USER)).thenReturn(Stream.of(message1, message2));
-        when(messageFormatter.format(message1)).thenReturn("message one");
-        when(messageFormatter.format(message2)).thenReturn("message two");
-        ReadCommand command = new ReadCommand("user", messageRepository, userRepository, messageFormatter, output);
+        User user = new User("user");
+        Message message1 = new Message(user, TEXT, AN_INSTANT_IN_TIME),
+                message2 = new Message(user, TEXT, ANOTHER_INSTANT_IN_TIME);
 
+        when(userRepository.find("user")).thenReturn(Optional.of(user));
+        when(messageRepository.allMessagesPostedTo(user)).thenReturn(Stream.of(message1, message2));
+        when(messageFormatter.format(message1)).thenReturn("message 1 formatted");
+        when(messageFormatter.format(message2)).thenReturn("message 2 formatted");
+
+        ReadCommand command = readCommandForUserName("user");
         command.execute();
 
         InOrder inOrder = inOrder(output);
-        inOrder.verify(output).writeLine("message one");
-        inOrder.verify(output).writeLine("message two");
+        inOrder.verify(output).writeLine("message 1 formatted");
+        inOrder.verify(output).writeLine("message 2 formatted");
     }
 
     @Test
     public void print_nothing_if_user_not_found() {
-        when(userRepository.find("user")).thenReturn(empty());
-        ReadCommand command = new ReadCommand("user", messageRepository, userRepository, messageFormatter, output);
+        when(userRepository.find("user")).thenReturn(Optional.empty());
+        ReadCommand command = readCommandForUserName("user");
 
         command.execute();
 
         verify(output, never()).writeLine(anyString());
     }
 
+    private ReadCommand readCommandForUserName(String userName) {
+        return new ReadCommand(userName, messageRepository, userRepository, messageFormatter, output);
+    }
 }
