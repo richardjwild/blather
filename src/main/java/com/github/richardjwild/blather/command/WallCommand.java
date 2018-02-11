859192c5a534aba845f0fd52cae0550dfabdf58c
Richard Wild <richardwild76@gmail.com>
2018-02-11 16:05:43 +0000

    Prepare formatted messages in WallCommand constructor

--- src/main/java/com/github/richardjwild/blather/command/WallCommand.java
+++ src/main/java/com/github/richardjwild/blather/command/WallCommand.java
@@ -1,24 +1,20 @@
 package com.github.richardjwild.blather.command;
 
 import com.github.richardjwild.blather.io.Output;
-import com.github.richardjwild.blather.message.Message;
 import com.github.richardjwild.blather.message.MessageRepository;
 import com.github.richardjwild.blather.time.TimestampFormatter;
 import com.github.richardjwild.blather.user.User;
 import com.github.richardjwild.blather.user.UserRepository;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
 
 public class WallCommand implements Command {
 
-    private final String followerUserName;
-    private final UserRepository userRepository;
-    private final MessageRepository messageRepository;
-    private final TimestampFormatter timestampFormatter;
     private final Output output;
+    private final Stream<String> formattedMessages;
 
     public WallCommand(
             String followerUserName,
@@ -27,32 +23,21 @@ public class WallCommand implements Command {
             TimestampFormatter timestampFormatter,
             Output output)
     {
-        this.followerUserName = followerUserName;
-        this.userRepository = userRepository;
-        this.messageRepository = messageRepository;
-        this.timestampFormatter = timestampFormatter;
         this.output = output;
+
+        Stream<User> wallUsers = userRepository.find(followerUserName)
+                .map(User::wallUsers)
+                .orElseGet(Stream::empty);
+
+        formattedMessages = wallUsers
+                .flatMap(messageRepository::allMessagesPostedTo)
+                .sorted()
+                .map(message -> message.formatWall(timestampFormatter));
     }
 
     @Override
     public void execute() {
-        findFollower().ifPresent(this::printWallMessages);
-    }
-
-    private Optional<User> findFollower() {
-        return userRepository.find(followerUserName);
-    }
-
-    private void printWallMessages(User follower) {
-        follower.wallUsers()
-                .flatMap(messageRepository::allMessagesPostedTo)
-                .sorted()
-                .map(this::format)
-                .forEach(output::writeLine);
-    }
-
-    private String format(Message message) {
-        return message.formatWall(timestampFormatter);
+        formattedMessages.forEach(output::writeLine);
     }
 
     @Override
