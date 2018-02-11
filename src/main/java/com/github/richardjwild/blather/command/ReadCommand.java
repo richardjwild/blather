97b3852634e8bd4c304d0e06fdf34caed753f9e3
Richard Wild <richardwild76@gmail.com>
2018-02-11 16:05:26 +0000

    Prepare formatted messages in ReadCommand constructor

--- src/main/java/com/github/richardjwild/blather/command/ReadCommand.java
+++ src/main/java/com/github/richardjwild/blather/command/ReadCommand.java
@@ -2,23 +2,19 @@ package com.github.richardjwild.blather.command;
 
 import com.github.richardjwild.blather.message.Message;
 import com.github.richardjwild.blather.message.MessageRepository;
-import com.github.richardjwild.blather.user.User;
 import com.github.richardjwild.blather.user.UserRepository;
 import com.github.richardjwild.blather.io.Output;
 import com.github.richardjwild.blather.time.TimestampFormatter;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
 
 public class ReadCommand implements Command {
 
-    private final String recipientUserName;
-    private final MessageRepository messageRepository;
-    private final UserRepository userRepository;
-    private final TimestampFormatter timestampFormatter;
     private final Output output;
+    private final Stream<String> formattedMessages;
 
     public ReadCommand(
             String recipientUserName,
@@ -27,31 +23,20 @@ public class ReadCommand implements Command {
             TimestampFormatter timestampFormatter,
             Output output)
     {
-        this.recipientUserName = recipientUserName;
-        this.messageRepository = messageRepository;
-        this.userRepository = userRepository;
-        this.timestampFormatter = timestampFormatter;
         this.output = output;
+
+        Stream<Message> messages = userRepository.find(recipientUserName)
+                .map(messageRepository::allMessagesPostedTo)
+                .orElseGet(Stream::empty);
+
+        formattedMessages = messages
+                .sorted()
+                .map(message -> message.formatRead(timestampFormatter));
     }
 
     @Override
     public void execute() {
-        findRecipient().ifPresent(this::printAllMessagesPostedToRecipient);
-    }
-
-    private Optional<User> findRecipient() {
-        return userRepository.find(recipientUserName);
-    }
-
-    private void printAllMessagesPostedToRecipient(User recipient) {
-        messageRepository.allMessagesPostedTo(recipient)
-                .sorted()
-                .map(this::format)
-                .forEach(output::writeLine);
-    }
-
-    private String format(Message message) {
-        return message.formatRead(timestampFormatter);
+        formattedMessages.forEach(output::writeLine);
     }
 
     @Override
